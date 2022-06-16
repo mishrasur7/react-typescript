aaad871f494efe39f03172c66bc6e7d14ead5bd7
Suraj Mishra <mishrasur7@gmail.com>
2022-06-16 18:29:15 +0300

    some types tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 
 function App() {
-  let amount:number = 10
-  let item:string = 'apple'
-  let unitPrice:number = 2
-  let totalPrice:number = amount* unitPrice
+  // let amount:number = 10
+  // let item:string = 'apple'
+  // let unitPrice:number = 2
+  // let totalPrice:number = amount* unitPrice
 
-  function myFuntion(parameterA:number, parameterB:number)  {
-    console.log(parameterA + parameterB)
-  }
-  myFuntion(5, 15)
+  // function myFuntion(parameterA:number, parameterB:number)  {
+  //   console.log(parameterA + parameterB)
+  // }
+  // myFuntion(5, 15)
 
   
-  const test = (function yourFunction() {
-    setTimeout(() => console.log('suraj'), 3000)
-  })()
+  // const test = (function yourFunction() {
+  //   setTimeout(() => console.log('suraj'), 3000)
+  // })()
+
+  // let str: Object = 'string type'
+  // str.toUpperCase() //error
+
+
+// function getPro<Type, Key extends keyof Type> (obj: Type, key: Key) {
+//   return obj[key]
+// }
+// let x = {a:1, b: 2, c: 3, d: 4}
+// const a = getPro(x, 'm')
+// console.log(a)
+
+// type Arrayish = {[n: number]: unknown} 
+//   type A = keyof Arrayish
+// console.log(type A)
+
+
  
 
   return (
     <div className="App">
-      <h1>Product: {item}</h1>
+      {/* <h1>Product: {item}</h1>
       <h2>Unit price: {unitPrice}</h2>
-      <h2>Total Price: {totalPrice}</h2>
+      <h2>Total Price: {totalPrice}</h2> */}
     </div>
   );
 }
